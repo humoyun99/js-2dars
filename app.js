55e1fde1a075eb46dcbbe7963460bf000a64fc68
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-14 15:05:24 +0500

    3 uzgarish

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 const ism=prompt("ismingizni kiriting kiriting:")
 const yosh=Number(prompt('yoshingizni kiriting:'))
+const result=`Qadirli  ${ism}. siz ${2024-yosh} yida tugilgansiz`
+alert(result)
